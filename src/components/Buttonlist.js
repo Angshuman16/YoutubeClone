20f6fe512bcc5933e36a937168eddc0ec251b314
Angshuman16 <pathakangshu@gmail.com>
2023-04-06 01:20:41 +0530

    Changed the ButtonList and Buttons

--- src/components/Buttonlist.js
+++ src/components/Buttonlist.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import Button from './Button'
+import { popular } from '../Constants'
 
 const Buttonlist = () => {
   return (
-    <div className='flex'>
-      <Button name="All" />
-      <Button name="Gaming" />
-      <Button name="Food" />
-      <Button name="Comedy" />
-      <Button name="Movies" />
-      <Button name="tmkoc" />
-      <Button name="Live" />
-      <Button name="Programming" />
-      <Button name="Mixes" />
-      <Button name="Music" />
-       
-      <Button name="FlyingBeast" />
-            
+    <>
+    <div className='flex overflow-hidden '>
+     
       
+    {
 
-
+      Object.values(popular).map(items =>{
+        return(
+          <Button name={items.name} />
+        )
+      })
+       
+    }
+    {/* <Button name= "App" />
+    <Button name= "Movies" /> */}
     </div>
+    </>
   )
 }
 
