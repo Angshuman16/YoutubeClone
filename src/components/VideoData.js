0250769dc89ce22fc026c783a189a124b508e4f0
Angshuman16 <pathakangshu@gmail.com>
2023-07-01 09:00:08 +0530

    Node changes

--- src/components/VideoData.js
+++ src/components/VideoData.js
@@ -15,7 +15,7 @@ const VideoData = ({data}) => {
       setLikeCount(data?.statistics?.likeCount);
       console.log(LikeCount);
       
-    }, [LikeCount]);
+    }, [data?.statistics?.likeCount]);
 
     
 
